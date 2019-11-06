2a8631606288dddf2cb50ccd8763918ba5ffd11a
Enric Girona <enricgirona93@gmail.com>
2019-11-06 19:28:05 +0100

    database connection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
+const app = express();
 const cors = require("cors");
-app.use(bodyParser.json());
+const db = require("./keys").mongoURI;
+const mongoose = require("mongoose");
+
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+app.use(bodyParser.json());
 app.use(cors());
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
+
+app.use("/cities", require("./routes/cities"));
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("Connection to Mongo DB established"))
+  .catch(err => console.log(err));
